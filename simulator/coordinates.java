136ed7ec304be4b9b824cf89abe5ff0dfc95012f
Chace GREEN <chgreen@e4r7p3.wethinkcode.co.za>
2018-06-25 12:47:31 +0200

    coordinates frame

---
+++ simulator/coordinates.java
@@ -0,0 +1,23 @@
+package simulator;
+
+public class Coordinates{
+    private int longitude;
+    private int latitude;
+    private int height;
+
+    public Coordinates(int longitude, int latitude, int height){
+
+    }
+
+    public getLongitude(){
+
+    }
+
+    public getLatitude(){
+        
+    }
+
+    public getHeight(){
+        
+    }
+}
