1f12807aab34c78a2e4c65075e428d1a8b3ede59
Chace GREEN <chgreen@e4r7p3.wethinkcode.co.za>
2018-06-25 13:29:30 +0200

    push again

---
+++ simulator/Tower.java
@@ -0,0 +1,16 @@
+package simulator;
+
+import simulator.vehicles.Flyable;
+
+public class Tower {
+
+    private List<Flyable> observers = new ArrayList<Flyable>();
+
+    public void register(Flyable flyable){
+
+    }
+
+    public void unregister(Flyable flyable){
+
+    }
+}
---
+++ simulator/WeatherTower.java
@@ -0,0 +1,15 @@
+package simulator;
+
+import simulator.Coordinates;
+
+public class WeatherTower{
+
+    public String getWeather(Coordinates coordinates){
+
+    }
+
+    void changeWeather(){
+        
+    }
+
+}
