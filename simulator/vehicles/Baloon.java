7a30bb4feaa2943217e29422e100e9f4dcb09da0
Chace GREEN <chgreen@e4r7p3.wethinkcode.co.za>
2018-06-25 10:54:10 +0200

    added properties to vehicle classes

--- simulator/vehicles/Baloon.java
+++ simulator/vehicles/Baloon.java
@@ -1,5 +1,16 @@
 package simulator.vehicles;
 
 public class Baloon extends Aircraft{
-    
+    private WeatherTower weatherTower;
+
+    Baloon(String name, Coordinates coordinates){
+        Super(name, coordinates);
+    }
+
+    public void updateConditions(){
+
+    }
+
+    public void registerTower(WeatherTower weatherTower){
+    }
 }
--- simulator/vehicles/Helicopter.java
+++ simulator/vehicles/Helicopter.java
@@ -1,5 +1,16 @@
 package simulator.vehicles;
 
 public class Helicopter extends Aircraft{
-    
+    private WeatherTower weatherTower;
+
+    Helicopter(String name, Coordinates coordinates){
+        Super(name, coordinates);
+    }
+
+    public void updateConditions(){
+
+    }
+
+    public void registerTower(WeatherTower weatherTower){
+    }
 }
